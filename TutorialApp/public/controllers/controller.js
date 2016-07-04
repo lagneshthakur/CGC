b0068745d7358160f0cfc72e2c3560bbb8f92d20
Lagnesh Thakur <lagneshthakur@gmail.com>
2016-07-04 16:03:34 +0530

    Connected to MongoDB to GET for retrieval and Post for insert

--- TutorialApp/public/controllers/controller.js
+++ TutorialApp/public/controllers/controller.js
@@ -3,9 +3,21 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
 	console.log("Hello World from controller");
 
-	$http.get('/contactlist').success(function(response){
-		console.log("I got the data i requested");
-		$scope.contactlist = response;
-	})
+	var refresh = function(){
+		$http.get('/contactlist').success(function(response){
+			console.log("I got the data i requested");
+			$scope.contactlist = response;
+			
+		});
+	};
+
+	refresh();
+	$scope.addContact = function() {
+		console.log($scope.contact);
+		$http.post('/contactlist',$scope.contact).success(function(response){
+			console.log(response);
+			refresh();
+		});
+	}
 		
 }]);
--- TutorialApp/server.js
+++ TutorialApp/server.js
@@ -1,27 +1,24 @@
 var express =	require('express');
+var mongojs = require('mongojs');
 var app = express();
+var db = mongojs('contactlist',['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
+
 app.get('/contactlist',function(req,res){
 	console.log("I received a get request");
-	person1 = {
-		name: 'Tim',
-		email: 'tim@email.com',
-		number: '(111) 111-1111'
-	};
-	person2 = {
-		name: 'John',
-		email: 'john@email2.com',
-		number: '(222) 222-2222'
-	};
-	person3 = {
-		name: 'Emily',
-		email: 'emily@email3.com',
-		number: '(333) 333-3333'
-	};
-
-	var contactlist = [person1,person2,person3];
-	res.json(contactlist);	
-})
+	db.contactlist.find(function(err,docs){
+		console.log(docs);
+		res.json(docs);
+	})
+});
+app.post('/contactlist',function(req,res){
+	console.log(req.body);
+	db.contactlist.insert(req.body,function(err,doc){
+		res.json(doc);
+	});
+});
 app.listen(3000);
 console.log("Server running on port 3000");
