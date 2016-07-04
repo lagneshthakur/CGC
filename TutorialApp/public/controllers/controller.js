5b7f50921ad74719a736f9b2643c30a40c0b3ec6
Lagnesh Thakur <lagneshthakur@gmail.com>
2016-07-04 17:43:30 +0530

    Complete working CRUD

--- TutorialApp/public/controllers/controller.js
+++ TutorialApp/public/controllers/controller.js
@@ -7,7 +7,7 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
 		$http.get('/contactlist').success(function(response){
 			console.log("I got the data i requested");
 			$scope.contactlist = response;
-			
+			$scope.contact = "";
 		});
 	};
 
@@ -18,6 +18,26 @@ myApp.controller('AppCtrl', ['$scope', '$http', function ($scope, $http) {
 			console.log(response);
 			refresh();
 		});
-	}
-		
+	};
+
+	$scope.remove = function(id){
+		console.log(id);
+		$http.delete('/contactlist/' + id).success(function(response){
+			refresh();
+		});
+	};
+	
+	$scope.edit = function(id){
+		console.log(id);
+		$http.get('/contactlist/' + id).success(function(response){
+			$scope.contact = response;
+		})
+	};
+
+	$scope.update = function(){
+		console.log($scope.contact._id);
+		$http.put('/contactlist/' + $scope.contact._id, $scope.contact).success(function(response){
+			refresh();
+		})
+	};
 }]);
--- TutorialApp/server.js
+++ TutorialApp/server.js
@@ -20,5 +20,31 @@ app.post('/contactlist',function(req,res){
 		res.json(doc);
 	});
 });
+
+app.delete('/contactlist/:id',function(req,res){
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.remove({_id: mongojs.ObjectId(id)},function(err, docs){
+		res.json(docs);
+	})
+});
+
+app.get('/contactlist/:id',function(req,res){
+	var id = req.params.id;
+	console.log(id);
+	db.contactlist.findOne({_id:mongojs.ObjectId(id)},function(err,doc){
+		res.json(doc);
+	})
+});
+
+app.put('/contactlist/:id',function(req,res){
+	var id = req.params.id;
+	console.log(req.body.name);
+	db.contactlist.findAndModify({query: { _id:mongojs.ObjectId(id)},
+		update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+		new: true}, function(err,doc){
+			res.json(doc);
+		});
+});
 app.listen(3000);
 console.log("Server running on port 3000");
