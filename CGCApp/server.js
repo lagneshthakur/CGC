39e9e0d9b79a8d3bc009b57ebd2efccb005d1cd4
Lagnesh Thakur <lagneshthakur@gmail.com>
2016-07-18 13:44:01 +0530

    Improved follow & unfollow, Get list of following, Initial DB dump

--- CGCApp/server.js
+++ CGCApp/server.js
@@ -40,25 +40,78 @@ app.get('/posts',function(req,res){
 	})
 });
 
+//Follow a user
 app.post('/follow',function(req,res){
-	console.log("I received a post request");
-
+	console.log("I received a post request on follow");
+	// Add follower to the user who is to be followed by current user
+	User.findOne({ _id:req.body.id},function(err,doc){
+		User.update({_id: req.body.user_id}, {
+		$addToSet: {
+			follower: { _id : doc._id, name:{ first: doc.name.first , last: doc.name.last }} 
+		}
+		},function(err,affected,resp){
+			console.log(affected);
+		});
+	});
+	// Add following to the current user
 	User.findOne({ _id:req.body.user_id},function(err,doc){
 		User.update({_id: req.body.id}, {
 		$addToSet: {
 			following: { _id : doc._id, name:{ first: doc.name.first , last: doc.name.last }} 
-		} 
+		}
+		}, function(err, affected, resp) {
+			if(affected.ok)
+			{
+					if(affected.nModified)
+					{
+						res.send("Successfuly Followed!");
+					}
+					else{
+						res.send("Already Followed!");
+					}
+			}
+		});
+	});
+});
+
+//Unfollow a user
+app.delete('/follow',function(req,res){
+	console.log("I received a delete request on follow");
+	// Remove from unfollowed user's followers list
+			User.update({_id: req.body.user_id}, {
+		$pull: {
+			follower: { _id : req.body.id}} 
+		}, function(err, affected, resp) {
+
+		});
+
+	// Remove from current user's following list
+		User.update({_id: req.body.id}, {
+		$pull: {
+			following: { _id : req.body.user_id}} 
 		}, function(err, affected, resp) {
 		   if(affected.ok)
 		   {
 		   		if(affected.nModified)
 		   		{
-		   			res.send("Successfuly Followed!");
+		   			res.send("Successfuly Unfollowed!");
 		   		}
-		   		res.send("Already Followed!");
+		   		else{
+		   			res.send("Already not following!");
+		   		}
 		   }
 		})
-	});
+});
+
+app.get('/following/:id',function(req,res){
+	console.log("Get request on following");
+	User.findById(req.params.id,function(err,user){
+		var following = [];
+		for (var i = 0; i < user.following.length; i++) {
+			following.push(user.following[i].name);
+		}
+		res.send(following);
+	})
 });
 
 http.listen(3000);
--- CGCApp/test/test.js
+++ CGCApp/test/test.js
@@ -2,8 +2,8 @@ var request = require('superagent');
 var expect = require('expect.js');
 
 var test_data = {
-	"id": "577ca8a21154ca1433e91572",
-	"user_id": "577ca9191154ca1433e91574"
+	"id": "577ca9191154ca1433e91574",
+	"user_id": "577ce7041154ca1433e9157e"
 }
 
 describe('Following Other Users',function(){
@@ -13,6 +13,32 @@ describe('Following Other Users',function(){
 			expect(res).to.exist;
 
 			expect(res.text).to.contain("Success");
+
+			done();
+		});
+	});
+});
+
+describe.only('Unfollowing a User',function(){
+	it('Check response on unfollowing a user',function(done){
+		request.delete('localhost:3000/follow', test_data).end(function(err,res){
+			//Check the response is okay or not
+			expect(res).to.exist;
+
+			expect(res.text).to.contain("Success");
+
+			done();
+		});
+	});
+});
+
+describe('Get list of following',function(){
+	it('Check response on requesting list of users that I am following',function(done){
+		request.get('localhost:3000/following/' + '577ca8a21154ca1433e91572' ).end(function(err,res){
+			//Check the response is okay or not
+			expect(res).to.exist;
+			array = JSON.parse(res.text);
+			expect(array).to.be.an('array');
 
 			done();
 		});
